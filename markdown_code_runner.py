b973e1c4de4c88f94b9f1bf128b5a726bf46f75d
Bas Nijholt <bas@nijho.lt>
2023-04-02 13:45:34 -0700

    Test cmdline interface

--- markdown_code_runner.py
+++ markdown_code_runner.py
@@ -1,13 +1,17 @@
 # Copyright (c) 2023, Bas Nijholt
 # All rights reserved.
-"""Automatically update Markdown files with code block output.
+"""Markdown Code Runner.
+
+Automatically update Markdown files with code block output.
+
+This script is part of the 'markdown-code-runner' package available on GitHub:
+https://github.com/basnijholt/markdown-code-runner
 
 Add code blocks between <!-- START_CODE --> and <!-- END_CODE --> in your Markdown file.
 The output will be inserted between <!-- START_OUTPUT --> and <!-- END_OUTPUT -->.
 
 Example:
 -------
-```
 <!-- START_CODE -->
 <!-- print('Hello, world!') -->
 <!-- END_CODE -->
@@ -19,6 +23,7 @@ This will be replaced by the output of the code block above.
 """
 from __future__ import annotations
 
+import argparse
 import contextlib
 import io
 from pathlib import Path
@@ -97,17 +102,62 @@ def process_markdown(content: list[str]) -> list[str]:
     return new_lines
 
 
-def update_markdown_file(filepath: Path) -> None:
+def update_markdown_file(
+    input_filepath: Path | str,
+    output_filepath: Path | str | None = None,
+    *,
+    debug: bool = False,
+) -> None:
     """Rewrite a Markdown file by executing and updating code blocks."""
-    with filepath.open() as f:
+    if isinstance(input_filepath, str):
+        input_filepath = Path(input_filepath)
+    with input_filepath.open() as f:
         original_lines = [line.rstrip("\n") for line in f.readlines()]
-
+    if debug:
+        print(f"Processing input file: {input_filepath}")
     new_lines = process_markdown(original_lines)
     updated_content = "\n".join(new_lines).rstrip() + "\n"
-
-    with filepath.open("w") as f:
+    if debug:
+        print(f"Writing output to: {output_filepath}")
+    output_filepath = (
+        input_filepath if output_filepath is None else Path(output_filepath)
+    )
+    with output_filepath.open("w") as f:
         f.write(updated_content)
+    if debug:
+        print("Done!")
+
+
+def main() -> None:
+    """Parse command line arguments and run the script."""
+    parser = argparse.ArgumentParser(
+        description="Automatically update Markdown files with code block output.",
+    )
+    parser.add_argument(
+        "input",
+        type=str,
+        help="Path to the input Markdown file.",
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        type=str,
+        help="Path to the output Markdown file. (default: overwrite input file)",
+        default=None,
+    )
+    parser.add_argument(
+        "-d",
+        "--debug",
+        action="store_true",
+        help="Enable debugging mode (default: False)",
+    )
+
+    args = parser.parse_args()
+
+    input_filepath = Path(args.input)
+    output_filepath = Path(args.output) if args.output is not None else input_filepath
+    update_markdown_file(input_filepath, output_filepath, debug=args.debug)
 
 
 if __name__ == "__main__":
-    update_markdown_file(Path(__file__).parent.parent / "README.md")
+    main()
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,8 +1,28 @@
-from markdown_code_runner import MARKERS, md_comment, process_markdown
+"""Test the markdown_code_runner app."""
+from __future__ import annotations
+
+import argparse
+from pathlib import Path
+from unittest.mock import patch
+
+import pytest
+
+from markdown_code_runner import (
+    MARKERS,
+    execute_code_block,
+    main,
+    md_comment,
+    process_markdown,
+    remove_md_comment,
+)
+
+TEST_FOLDER = Path(__file__).parent
 
 
-def test_process_markdown():
-    def assert_process(input_lines, expected_output):
+def test_process_markdown() -> None:
+    """Test the process_markdown function."""
+
+    def assert_process(input_lines: list[str], expected_output: list[str]) -> None:
         output = process_markdown(input_lines)
         assert output == expected_output, f"Expected {expected_output}, got {output}"
 
@@ -80,3 +100,84 @@ def test_process_markdown():
         "More text",
     ]
     assert_process(input_lines, expected_output)
+
+
+def test_remove_md_comment() -> None:
+    """Test the remove_md_comment function."""
+    input_str = "<!-- This is a comment -->"
+    output_str = remove_md_comment(input_str)
+    assert (
+        output_str == "This is a comment"
+    ), f"Expected 'This is a comment', got '{output_str}'"
+
+    input_str = "This is not a comment"
+    with pytest.raises(ValueError, match="Invalid Markdown comment format"):
+        output_str = remove_md_comment(input_str)
+
+
+def test_execute_code_block() -> None:
+    """Test the execute_code_block function."""
+    code = ["print('Hello, world!')"]
+    output = execute_code_block(code)
+    expected_output = ["Hello, world!", ""]
+    assert output == expected_output, f"Expected {expected_output}, got {output}"
+
+
+def test_main_no_arguments(tmp_path: Path) -> None:
+    """Test the main function with no arguments."""
+    test_filepath = Path("tests/test.md")
+    output_filepath = tmp_path / "output.md"
+    expected_output_filepath = Path("tests/expected_output.md")
+
+    with patch(
+        "argparse.ArgumentParser.parse_args",
+        return_value=argparse.Namespace(input=test_filepath, output=None, debug=False),
+    ):
+        main()
+        assert output_filepath.exists() is False
+        with test_filepath.open() as f1, expected_output_filepath.open() as f2:
+            assert f1.read() == f2.read()
+
+
+def test_main_filepath_argument(tmp_path: Path) -> None:
+    """Test the main function with a filepath argument."""
+    test_filepath = Path("tests/test.md")
+    output_filepath = tmp_path / "output.md"
+    expected_output_filepath = Path("tests/expected_output.md")
+
+    with patch(
+        "argparse.ArgumentParser.parse_args",
+        return_value=argparse.Namespace(
+            input=test_filepath,
+            output=str(output_filepath),
+            debug=False,
+        ),
+    ):
+        main()
+        assert output_filepath.exists()
+        with output_filepath.open() as f1, expected_output_filepath.open() as f2:
+            assert f1.read() == f2.read()
+
+
+def test_main_debug_mode(capfd: pytest.CaptureFixture, tmp_path: Path) -> None:
+    """Test the main function with debug mode enabled."""
+    test_filepath = Path("tests/test.md")
+    output_filepath = tmp_path / "output.md"
+    expected_output_filepath = Path("tests/expected_output.md")
+
+    with patch(
+        "argparse.ArgumentParser.parse_args",
+        return_value=argparse.Namespace(
+            input=test_filepath,
+            output=str(output_filepath),
+            debug=True,
+        ),
+    ):
+        main()
+        assert output_filepath.exists()
+        with output_filepath.open() as f1, expected_output_filepath.open() as f2:
+            assert f1.read() == f2.read()
+        captured = capfd.readouterr()
+        assert f"Processing input file: {test_filepath}" in captured.out
+        assert f"Writing output to: {output_filepath}" in captured.out
+        assert "Done!" in captured.out
