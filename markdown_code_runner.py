70f3b3bea439f04280a53c186be65b9a83239091
Bas Nijholt <bas@nijho.lt>
2023-04-04 11:36:57 -0700

    Allow triple backtick code blocks (#4)

--- markdown_code_runner.py
+++ markdown_code_runner.py
@@ -20,12 +20,20 @@ This will be replaced by the output of the code block above.
 
 <!-- END_OUTPUT -->
 ```
+Alternatively, you can add a <!-- SKIP --> comment to skip a code block.
+
+Another way is to run code blocks in triple backticks:
+```python markdown-code-runner
+print('Hello, world!')
+```
+Which will print the output of the code block above below the code block.
 """
 from __future__ import annotations
 
 import argparse
 import contextlib
 import io
+import re
 from pathlib import Path
 
 import pkg_resources
@@ -58,11 +66,14 @@ def remove_md_comment(commented_text: str) -> str:
     return commented_text[5:-4]
 
 
-def execute_code_block(code: list[str]) -> list[str]:
+def execute_code_block(code: list[str], *, verbose: bool = False) -> list[str]:
     """Execute a code block and return its output as a list of strings."""
     f = io.StringIO()
+    full_code = "\n".join(code)
+    if verbose:
+        print(f"Executing code block:\n{full_code}\n")
     with contextlib.redirect_stdout(f):
-        exec("\n".join(code))  # noqa: S102
+        exec(full_code)  # noqa: S102
     return f.getvalue().split("\n")
 
 
@@ -78,7 +89,7 @@ def _bold(text: str) -> str:
     return f"{bold}{text}{reset}"
 
 
-def process_markdown(  # noqa: PLR0912
+def process_markdown(  # noqa: PLR0912, PLR0915
     content: list[str],
     *,
     verbose: bool = False,
@@ -101,13 +112,19 @@ def process_markdown(  # noqa: PLR0912
     new_lines = []
     code: list[str] = []
     original_output: list[str] = []
-    in_code_block = in_output_block = skip_code_block = False
+    in_code_block = in_backtick_code_block = in_output_block = skip_code_block = False
     output: list[str] | None = None
 
+    triple_backtick_pattern = re.compile(r"^```python markdown-code-runner")
+    content = [
+        *content,
+        "",
+    ]  # add empty line to process last code block (if at end of file)
     for i, line in enumerate(content):
         if verbose:
             nr = _bold(f"line {i:4d}")
             print(f"{nr}: {line}")
+
         if is_marker(line, "skip"):
             skip_code_block = True
         elif is_marker(line, "start_code"):
@@ -133,16 +150,26 @@ def process_markdown(  # noqa: PLR0912
             if is_marker(line, "end_code"):
                 in_code_block = False
                 if not skip_code_block:
-                    output = execute_code_block(code)
+                    output = execute_code_block(code, verbose=verbose)
                 code = []
             else:
                 code.append(remove_md_comment(line))
         elif in_output_block:
             original_output.append(line)
+        elif in_backtick_code_block:
+            if line.strip() == "```":
+                in_backtick_code_block = False
+                if not skip_code_block:
+                    output = execute_code_block(code, verbose=verbose)
+                code = []
+            else:
+                code.append(line)
+        elif triple_backtick_pattern.match(line):
+            in_backtick_code_block = True
 
-        if not in_output_block:
+        last_line = i == len(content) - 1
+        if not in_output_block and not last_line:
             new_lines.append(line)
-
     return new_lines
 
 
